1d86ca7feb8d8a62894e0b10e83a4fea84a3da0a
noRan56 <noran.nassef12@gmail.com>
2025-05-16 15:08:57 +0300

    refactor

--- lib/feature/presentation_layer/view_model/my_order_view_model.dart
+++ lib/feature/presentation_layer/view_model/my_order_view_model.dart
@@ -2,30 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:puresoftinternproject/core/shared_widget/text_widget_font_14_grey.dart';
 import 'package:puresoftinternproject/core/styles/custom_app_colors.dart';
+import 'package:puresoftinternproject/feature/data_layer/model/my_order_model.dart';
 
 class MyOrderViewModel extends StatelessWidget {
   const MyOrderViewModel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<String> leading = [
-      'assets/images/delvering.png',
-      'assets/images/finishedIcon.png',
-      'assets/images/Cancelled.png',
-      'assets/images/Delivered.png',
-      'assets/images/workingIcon.png',
-      'assets/images/new.png',
-    ];
-    List<String> trailing = [
-      'assets/images/delveringArrow.png',
-      'assets/images/finishedArrow.png',
-      'assets/images/canceledArrow.png',
-      'assets/images/DeliveredArrow.png',
-      'assets/images/workingArrow.png',
-      'assets/images/newArrow.png',
-    ];
+    final data = MyOrderModel();
+
     return ListView.builder(
-      itemCount: leading.length,
+      itemCount: data.leading.length,
       itemBuilder: (context, index) {
         return Container(
           padding: EdgeInsets.only(bottom: 20.h),
@@ -36,9 +23,9 @@ class MyOrderViewModel extends StatelessWidget {
             color: CustomAppColors.white,
             elevation: 3,
             child: ListTile(
-              leading: Image.asset(leading[index]),
-              trailing: Image.asset(trailing[index]),
-              title: Text('Order 1'),
+              leading: Image.asset(data.leading[index]),
+              trailing: Image.asset(data.trailing[index]),
+              title: Text(data.orderTitle[index]),
               subtitle: Column(
                 children: [
                   Row(
